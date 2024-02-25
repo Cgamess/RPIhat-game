48c950b5850685d1948e9e3d1208283c7611dfa4
Cgamess <153407105+Cgamess@users.noreply.github.com>
2024-02-25 22:30:22 +0000

    Made code more proper

--- main.py
+++ main.py
@@ -10,9 +10,10 @@ import threading as th
 import reagons as rg
 import gamefuncts as gf
 from gamefuncts import tick
-if 0: logo.logofade()
-while True:
-  daypoint=int((time.time()%86400)//21600)
-  start_time = time.time()
-  tick()
-  print("FPS: ", 1.0 / (time.time() - start_time), len(gf.goblins), daypoint) # FPS = 1 / time to process loop
+if __name__ == "__main__":
+  if 0: logo.logofade()
+  while True:
+    daypoint=int((time.time()%86400)//21600)
+    start_time = time.time()
+    tick()
+    print("FPS: ", 1.0 / (time.time() - start_time), len(gf.goblins), daypoint) # FPS = 1 / time to process loop
