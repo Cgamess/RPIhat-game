3ac644e1a5260a159cb7a8e180de7771ed627a0d
Cgamess <153407105+Cgamess@users.noreply.github.com>
2024-02-26 11:12:23 -0700

    Create movement.py

---
+++ movement.py
@@ -0,0 +1,31 @@
+
+
+# Function to get joystick event
+def get_joystick_event():
+    for event in sense.stick.get_events():
+        if event.action == 'pressed' or event.action == 'held':
+            return event.direction
+    return None
+
+# Function to get movement matrix based on joystick direction
+def get_movement_matrix(direction):
+    if direction == 'up':
+        return [0, 1]
+    elif direction == 'down':
+        return [0, -1]
+    elif direction == 'left':
+        return [-1, 0]
+    elif direction == 'right':
+        return [1, 0]
+    elif direction == 'middle':
+        return [0, 0]
+    elif direction == 'up_left':
+        return [-1, 1]
+    elif direction == 'up_right':
+        return [1, 1]
+    elif direction == 'down_left':
+        return [-1, -1]
+    elif direction == 'down_right':
+        return [1, -1]
+    elif direction == 'pressed':
+        return None  # Handle button press
