1b6afc44f84d35a53f7c442ef59f080790d67fae
Cgamess <153407105+Cgamess@users.noreply.github.com>
2024-02-26 03:52:49 +0000

    fix sync.sh

--- sync.sh
+++ sync.sh
@@ -1,1 +1,10 @@
-git pull --rebase origin main
+git add .
+
+# Fetch the latest changes from the remote repository
+git fetch origin
+
+# Checkout the branch you want to sync (e.g., master, main)
+git checkout master
+
+# Pull changes from the remote repository to the local repository
+git pull origin master
