36ede5defb45b3576de340fd2911343694a75cb3
Cgamess <153407105+Cgamess@users.noreply.github.com>
2024-02-26 22:32:35 +0000

    weird thing with pi5

--- sync.sh
+++ sync.sh
@@ -1,1 +1,1 @@
-git merge origin main; git push -u origin main; git pull --rebase origin main
+git commit -a -m "weird thing with pi5"; git merge origin main; git push -u origin main; git pull --rebase origin main
