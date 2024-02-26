c2b29d2e2dbf6dfd879a6524fc70697fb366992d
Cgamess <153407105+Cgamess@users.noreply.github.com>
2024-02-26 10:03:14 -0700

    Update gamefuncts.py

--- gamefuncts.py
+++ gamefuncts.py
@@ -1,4 +1,5 @@
 from sense_hat import SenseHat as s
+import creaturebase as cb
 import functs, time
 import functs as f
 import math
@@ -64,25 +65,7 @@ def makealtani(loops,wait,alts,lenl=1):
     return(b)
 
 
-class Map:
-    cmx, cmy, cmz = 0, 0, 0
-    cworld = 0
-
-class BaseEntityModel(Map):
-    def __init__(self):
-        self.hp = 100
-        self.dead = False
-        self.anibuffer=[]
-        self.color = [0, 0, 0]
-        self.x, self.y, self.z, self.world = 0, 0, 0, 0
-        self.tx, self.ty, self.tz = 0, 0, 0
-        self.mx, self.my, self.mz = 0, 0, 0
-
-    def render(self):
-        if [self.mx, self.my, self.mz, self.world] == [super().cmx, super().cmy, super().cmz, super().cworld]:
-            sh.set_pixel(self.x,self.y,self.color)
-
-class Player(BaseEntityModel):
+class Player(cb.BaseEntityModel):
     def __init__(self):
         super().__init__()
         self.color = [255, 255, 255]
@@ -92,7 +75,7 @@ class Player(BaseEntityModel):
     def render(self):
         super().render()
 
-class Goblin(BaseEntityModel):
+class Goblin(cb.BaseEntityModel):
     def __init__(self):
         super().__init__()
         self.color = [0, 255, 0]
